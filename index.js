02784347c798a7b5013a2eefd6668c7d906da8f0
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-12 16:00:11 -0500

    Agregar el controlador de productos con detalles de categoria, consulta en formato json

--- index.js
+++ index.js
@@ -25,5 +25,17 @@ app.get('/categories', async (req, res) => {
 	}
 });
 
+app.get('/products',  (req, res) => {
+	pool.query(`SELECT p.id, p.name, p.description, p.price, p.currency,
+				p.quantity, p.active, p.created_date, p.update_date,
+				(SELECT ROW_TO_JSON(category_obj) FROM (
+					SELECT id, name FROM category WHERE id = p.category_id
+				) category_obj) AS category
+				FROM product AS p;`).then((result) => {
+		return res.status(200).json(result.rows);
+	}).catch(error => {
+		return res.status(500).json({error:error.message});
+	});
+});
 // console.log(process.env.DATABASE_URL);
 app.listen(port, () =>  console.log(`Server is running on port ${port}`));
