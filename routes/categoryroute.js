bef517a28edcb099cc1199ebc0fc245114db470d
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-14 18:29:04 -0500

    Agregar la ruta  DELETE /categorias/:id para eleminar una categoria

--- routes/categoryroute.js
+++ routes/categoryroute.js
@@ -1,11 +1,15 @@
 const app = require('express');
 // const router = require('express').Router();
-const { getAllCategories, createCategory, updateCategory } = require('../controllers/categoryController');
+const { getAllCategories,
+		createCategory,
+		updateCategory,
+		deleteCategory } = require('../controllers/categoryController');
 
 
 const router = app.Router();
 router.get('/categories', getAllCategories);
 router.post('/categories', createCategory);
 router.put('/categories/:id', updateCategory);
+router.delete('/categories/:id',deleteCategory)
 
 module.exports = router;
