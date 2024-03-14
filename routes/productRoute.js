27d2aa45a40f6ba68f4e9c7b23fb3be6763ab61b
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-14 17:51:10 -0500

    Agregar la ruta PUT /products/:id para actualizar un producto

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,9 @@
 const app = require('express');
-const { getAllProducts, createProduct } = require('../controllers/productController');
+const { getAllProducts, createProduct, updateProduct } = require('../controllers/productController');
 
 const router = app.Router();
 router.get('/products', getAllProducts );
 router.post('/products', createProduct);
+router.put('/products/:id', updateProduct);
 
 module.exports = router;
