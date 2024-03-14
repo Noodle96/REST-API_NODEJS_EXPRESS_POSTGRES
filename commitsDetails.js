f207489953d3b6aeebe1f7835b57e77f19024129
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-14 18:53:43 -0500

    Add details to commits in nodejs(Esto puede cambiar)

---
+++ commitsDetails.js
@@ -0,0 +1,15 @@
+const typeRoute = {
+	"GET": "Para obtener información",
+	"POST": "Para crear información",
+	"PUT": "Para actualizar información",
+	"DELETE": "Para eliminar información"
+}
+const detailsComits = [
+	{
+		"Controlador":"add a controller nameFunction to nameDetails",
+		"rutas": {
+			"1": "add a route [TypeRoute] /route/:id nameFunction",
+			"2": "add a route [TypeRoute] /route nameFunction",
+		},
+	}
+]
