84ad2e1611e956268dbd4f8fa595642073a01101
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-14 16:47:21 -0500

    Agregar la ruta PUT /categories/:id updateCategory

--- routes/categoryroute.js
+++ routes/categoryroute.js
@@ -1,10 +1,11 @@
 const app = require('express');
 // const router = require('express').Router();
-const { getAllCategories, createCategory } = require('../controllers/categoryController');
+const { getAllCategories, createCategory, updateCategory } = require('../controllers/categoryController');
 
 
 const router = app.Router();
 router.get('/categories', getAllCategories);
 router.post('/categories', createCategory);
+router.put('/categories/:id', updateCategory);
 
 module.exports = router;
