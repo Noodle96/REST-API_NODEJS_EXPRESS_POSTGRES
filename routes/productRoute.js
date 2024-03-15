24fe65b7b754c1bcd4ffe5e5ef646b5a65fc97e6
Noodle96 <jorgealfredomarino2@gmail.com>
2024-03-15 16:35:31 -0500

    Agregar la ruta GET /products/category/:categoryID

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,11 +3,13 @@ const { getAllProducts,
 		createProduct,
 		updateProduct,
 		deleteProduct, 
-		getProductById} = require('../controllers/productController');
+		getProductById,
+		getProductsByCategoryId} = require('../controllers/productController');
 
 const router = app.Router();
 router.get('/', getAllProducts );
 router.get('/:id',getProductById)
+router.get('/category/:categoryID', getProductsByCategoryId);
 router.post('/', createProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct)
